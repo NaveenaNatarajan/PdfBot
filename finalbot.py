0acca9c0e79fe9c6c3c2623807c669dc5c9bb7a9
NaveenaNatarajan <navirajan2003@gmail.com>
2024-10-04 11:09:08 +0530

    "temperature"

--- finalbot.py
+++ finalbot.py
@@ -41,7 +41,7 @@ def get_conversational_chain():
     Answer the question as detailed as possible from the provided context, make sure to provide all the details. 
     If the answer is not in the provided context just say, "answer is not available in the context", don't provide a wrong answer.\n\n
     Context:\n {context}?\n Question: \n{question}\n Answer: """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.7)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
